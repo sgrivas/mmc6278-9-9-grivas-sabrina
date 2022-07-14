b0ef3b97668854d71b0b1a7423cfe01cdfc61da3
Sabrina Grivas <sgrivas1@gmail.com>
2022-07-14 12:46:55 -0400

    added template literals

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const form=document.querySelector('#weather-app form')
 const weatherEl=document.getElementById('weather')
 const h2=document.createElement('h2')
 
-form.addEventListener('submit', async function(e){
+form.addEventListener('submit', async (e) => {
     e.preventDefault();
-    const userQuery=this.search.value
+    const userQuery=form.search.value
     if (!userQuery) return
     const fetchURL = `https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=eaa52095952f3eaa180f48bf7902508c&q=${userQuery}`
     try {
@@ -39,42 +39,20 @@ const renderedCity = ({
     },
     dt
 }) => {
-    weatherEl.innerHTML=''
     form.search.value=''
-
-    h2.textContent=name+", "+country
-    weatherEl.appendChild(h2)
-
-    var map=document.createElement('a')
-    map.href=`https://www.google.com/maps/search/?api=1&query=${lat},${lon}`
-    map.target="_BLANK"
-    map.textContent="Click to view map"
-    weatherEl.appendChild(map)
-
-    var img=document.createElement('img')
-    img.src=`https://openweathermap.org/img/wn/${icon}@2x.png`
-    weatherEl.appendChild(img)
-
-    var condition=document.createElement('p')
-    condition.textContent=description
-    condition.style="text-transform: capitalize"
-    weatherEl.appendChild(condition)
-
-    var temp=document.createElement('p')
-    temp.textContent=`Current: ${temp}° F`
-    weatherEl.appendChild(temp)
-
-    var feelslike=document.createElement('p')
-    feelslike.textContent=`Feels like: ${feels_like}° F`
-    weatherEl.appendChild(feelslike)
-
-    var milliTime=(dt)*1000
-    var date = new Date(milliTime)
-    var timeString = date.toLocaleTimeString('en-US', {
+    const milliTime=(dt)*1000
+    const date = new Date(milliTime)
+    const timeString = date.toLocaleTimeString('en-US', {
         hour: 'numeric',
         minute: '2-digit'
     })
-    var lastUpdated=document.createElement('p')
-    lastUpdated.textContent=`Last updated: ${timeString}`
-    weatherEl.appendChild(lastUpdated)
+    weatherEl.innerHTML=`
+    <h2>${name}, ${country}</h2>
+    <a href=https://www.google.com/maps/search/?api=1&query=${lat},${lon} target="_blank">Click to view map</a>
+    <img src="https://openweathermap.org/img/wn/${icon}@2x.png">
+    <p>${description}</p>
+    <p>Current: ${temp}° F<p/>
+    <p>Feels like: ${feels_like}° F</p>
+    <p>Last updated: ${timeString}</p>
+    `
 }
